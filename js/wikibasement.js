4c0767a67887ed3bb8d97e139f89de657b12cdfd
WILD4 <zhongku0225@gmail.com>
2019-10-25 20:29:44 +0800

    完善链接

--- js/wikibasement.js
+++ js/wikibasement.js
@@ -1,8 +1,4 @@
-$(document).ready(function(){
-    header();
-    hover();
 
-})	
 
 function hover(){
 
@@ -32,7 +28,7 @@ function header(){
 
     let nav = $("#nav_id");
     const lis =[
-        ["../monster/monster_.html","敌人"],
+        ["../monsters/monsters_.html","敌人"],
         ["../challenges/challenges.html","挑战"],
     ]
     for(let i in lis){
@@ -94,6 +90,27 @@ function header(){
     }
 }
 
-function footer(){
-    
+function bottomTable(list){
+    let mainDiv = $(".items-list");
+    let table = $("<table style='width:100%; margin-top:1em; border:1px solid #999; font-size:90%; text-align:center;'></table>");
+    let tbody = $("<tbody></tbody>");
+    let tr1 = $("<tr></tr>");
+    let th1 = $('<th style="white-space: nowrap; background-color:#3366CC; padding:0.2em 0.5em;"> <i><a href="/wiki/Risk_of_Rain_2" title="Risk of Rain 2"><font color="gold">Risk of Rain 2</font></a></i></th>');
+    let tr2 = $("<tr></tr>");
+    let th2 = $('<td style="padding:0.2em 0.5em;"></td>');
+
+    for(let i in list){
+        let a = $("<a></a>");
+        if(list[i][0] != "")
+            a.attr("href",list[i][0]);
+        a.text(list[i][1]);
+        if(i != 0)
+            th2.append(" · ");
+        th2.append(a);
+    }
+
+    mainDiv.append(table);
+    table.append(tbody);
+    tbody.append(tr1).append(th1);
+    tbody.append(tr2).append(th2);
 }
